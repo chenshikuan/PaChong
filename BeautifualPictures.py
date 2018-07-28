e77de60d35d835940bf3769273ad2e7b6e3b7194
chenshikuan <chensk930318@gmail.com>
2018-07-28 21:58:07 +0800

    修改为多线程 还未确定最佳并发数 现在并发数为5

--- BeautifualPictures.py
+++ BeautifualPictures.py
@@ -8,9 +8,10 @@ import pymssql
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from multiprocessing import Pool
+import threading
 # 开发一个日志系统， 既要把日志输出到控制台， 还要写入日志文件
 import logging
+import time
 #用字典保存日志级别
 format_dict = {
    1: logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'),
@@ -53,98 +54,80 @@ class Logger():
     def getlog(self):
         return self.logger
 
+def get_url(web_url):
+    print('开始网页get请求')
+    # 创建chrome参数对象
+    opt = Options()
+    # 把chrome设置成无界面模式
+    opt.add_argument('--headless')
+    driver = webdriver.Chrome(options=opt)
+    driver.get(web_url)
+    #print(driver.page_source)
+    #self.scroll_down(driver=driver, times=3)  #执行网页下拉到底部操作，执行3次
+    print('开始获取所有a标签')
+    all_a = BeautifulSoup(driver.page_source, 'lxml').findAll('div', class_='co_content2')  #获取网页中的class为cV68d的所有div标签
 
-class BeautifulPicture():
+    for a in all_a:
+        herf = a("a")
+        logger.info('herfNumber:'+str(len(herf)))
+        i = 0
+        for h in herf:
+            if 'href' in h.attrs.keys():
+                name = h.text
+                threading.TIMEOUT_MAX = 30
+                t = threading.Thread(target=get_magn, args=(web_url+h.attrs['href'], name))
+                t.start()
+                while threading.active_count() > 5:
+                    #print('the numbers of current thread is ' + str(threading.active_count()))
+                    time.sleep(1)
 
-    def __init__(self):  #类的初始化操作
-        self.headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/22.0.1207.1 Safari/537.1'}  #给请求指定一个请求头来模拟chrome浏览器
-        self.web_url = 'http://www.dytt8.net'  #要访问的网页地址
-        self.infoDic={}
-        self.logger = Logger('log.txt', 1, 'kk').getlog()
-        #self.folder_path = 'D:\BeautifulPicture'  #设置图片要存放的文件目录
-        # 创建chrome参数对象
+    while threading.active_count() > 1:
+        time.sleep(1)
 
+def get_magn(url, name):
 
-    def get_URL(self):
-        print('开始网页get请求')
-        # 创建chrome参数对象
-        opt = Options()
-        # 把chrome设置成无界面模式
-        opt.add_argument('--headless')
-        driver = webdriver.Chrome(chrome_options=opt)
-        driver.get(self.web_url)
-        #self.scroll_down(driver=driver, times=3)  #执行网页下拉到底部操作，执行3次
-        print('开始获取所有a标签')
-        all_a = BeautifulSoup(driver.page_source, 'lxml').findAll('div', class_='co_content2')  #获取网页中的class为cV68d的所有div标签
-        p = Pool(5)
-        for a in all_a:
-            herf = a("a")
-            self.logger.info('herfNumber:'+str(len(herf)))
-            i=0
-
-            for h in herf:
-                if 'href' in h.attrs.keys():
-                    name = h.text
-                    p.apply_async(self.get_magn, args=(self.web_url+h.attrs['href'], name))
-                    self.logger.info('开始创建进程：'+name)
-            print('Waiting for all subprocesses done...')
-            p.close()
-            p.join()
-            print('All subprocesses done.')
-            pass
-
-    def get_magn(self, url, name):
-        #self.logger = Logger('log.txt', 1, 'kk').getlog()
-        self.logger.info(url)
-        opt = Options()
-        # 把chrome设置成无界面模式
-        opt.add_argument('--headless')
-        driver = webdriver.Chrome(chrome_options=opt)
+    print('the current thread\'name is '+threading.current_thread().name)
+    logger.info(url)
+    opt = Options()
+    # 把chrome设置成无界面模式
+    opt.add_argument('--headless')
+    driver = webdriver.Chrome(chrome_options=opt)
+    try:
         driver.get(url)
-        locator = (By.ID, "kw")
-        try:
-            ele = WebDriverWait(driver, 10).until(EC.presence_of_element_located(locator))
-            driver.find_element_by_id("kw").send_keys('abc')
-
-        except:
-            print("ele can't find")
-            return
-
-        try:
-            SoupTbody = BeautifulSoup(driver.page_source, 'lxml').find('div', class_='co_content8')
-            # filename = SoupTbody.find('p').contents[0].string
-            child = SoupTbody.find('a')
-            magnURL = child.attrs['href']
-            self.infoDic[name] = magnURL
-
-            self.logger.info('movieName:'+name+'--movieMagn:'+magnURL)
-            driver.quit()
-        except Exception as err:
-            print("A exception happened " + str(err))
+        SoupTbody = BeautifulSoup(driver.page_source, 'lxml').find('div', class_='co_content8')
+        child = SoupTbody.find('a')
+        magn_url = child.attrs['href']
+        infoDic[name] = magn_url
+        logger.info('movieName:'+name+'--movieMagn:'+magn_url)
+        driver.quit()
+    except Exception as err:
+        print("A exception happened " + str(err))
 
 
-    def insertDB(self):
-        try:
-            conn = pymssql.connect(host='127.0.0.1', user='sa', password='873196023', database='movie', charset='utf8')
-            cursor = conn.cursor()
-            for key in self.infoDic:
-                u=(key,self.infoDic[key])
-                result=[]
-                result.append(u)
-            sql = 'insert into T_movie(movieName,movieURL) values(%s,%s)'
-            cursor.executemany(sql, result)
-            conn.commit()
-        except Exception as err:
-            conn.rollback()
-            print("A sql'exception happened " + str(err))
-        finally:
-            conn.close()
+def insertDB():
+    result = []
+    for key in infoDic:
+            u=(key,infoDic[key])
+            result.append(u)
+    sql = 'insert into T_movie(movieName,movieURL) values(%s,%s)'
+    try:
+        conn = pymssql.connect(host='127.0.0.1', user='sa', password='873196023', database='movie', charset='utf8')
+        cursor = conn.cursor()
+        cursor.executemany(sql, result)
+        conn.commit()
+    except Exception as err:
+        conn.rollback()
+        print("A sql'exception happened " + str(err))
+    finally:
+        conn.close()
 
 
 
 
 
-b = BeautifulPicture()
-b.get_URL()
-b.insertDB()
-pass
+headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/22.0.1207.1 Safari/537.1'}  #给请求指定一个请求头来模拟chrome浏览器
+web_url = 'http://www.dytt8.net'  #要访问的网页地址
+infoDic={}
+logger = Logger('log.txt', 1, 'kk').getlog()
+get_url(web_url)
+
